edbfd683010d6e0857fe0b8cddc35ef156ecb6c9
jsl <laarjsws@student.jyu.fi>
2020-12-24 01:13:38 +0200

    added enemy class

--- RoguePaladins/RoguePaladins.cs
+++ RoguePaladins/RoguePaladins.cs
@@ -5,25 +5,35 @@ using Jypeli.Assets;
 using Jypeli.Controls;
 using Jypeli.Widgets;
 
+//@author Joni Laari
+
+
 public class RoguePaladins : PhysicsGame
+    
 {
     const double Nopeus = 200;
-    const double HyppyNopeus = 750;
-    const int RUUDUN_KOKO = 40;
+    const double HyppyNopeus = 500;
+    const int RUUDUN_KOKO = 50;
     
 
     PlatformCharacter pelaaja1;
 
     Image pelaajanKuva = LoadImage("jeanele");
-    Image tahtiKuva = LoadImage("tahti");
+    Image tahtiKuva = LoadImage("portal");
     Image vartijaKuva = LoadImage("vartija");
+    Image taustaKuva = LoadImage("level1background");
+
 
     SoundEffect maaliAani = LoadSoundEffect("maali");
+
+    int kenttaNro = 1;
 
     public override void Begin()
     {
         Gravity = new Vector(0, -1000);
-        int kenttaNro = 1;
+
+
+        MediaPlayer.Play("Honor_Bound");
 
         LuoKentta(kenttaNro);
         LisaaNappaimet();
@@ -31,21 +41,23 @@ public class RoguePaladins : PhysicsGame
         Camera.Follow(pelaaja1);
         Camera.ZoomFactor = 1.2;
         Camera.StayInLevel = true;
+
     }
 
-    void LuoKentta(int kenttaNro)
+
+    void LuoKentta(int kenttaNro) //ohjeet kentän rakentamiseen tekstistä
     {
-        TileMap kentta = TileMap.FromLevelAsset("kentta1");
+        TileMap kentta = TileMap.FromLevelAsset("kentta" + kenttaNro);
         kentta.SetTileMethod('#', LisaaTaso);
         kentta.SetTileMethod('*', LisaaTahti);
         kentta.SetTileMethod('N', LisaaPelaaja);
         kentta.SetTileMethod('V', LisaaVartija);
         kentta.Execute(RUUDUN_KOKO, RUUDUN_KOKO);
         Level.CreateBorders();
-        Level.Background.CreateGradient(Color.White, Color.Red);
+        Level.Background.CreateGradient(Color.Black, Color.Green);
     }
 
-    void LisaaTaso(Vector paikka, double leveys, double korkeus)
+    void LisaaTaso(Vector paikka, double leveys, double korkeus) //tasoelementti
     {
         PhysicsObject taso = PhysicsObject.CreateStaticObject(leveys, korkeus);
         taso.Position = paikka;
@@ -69,18 +81,39 @@ public class RoguePaladins : PhysicsGame
         pelaaja1.Position = paikka;
         pelaaja1.Mass = 4.0;
         pelaaja1.Image = pelaajanKuva;
+        pelaaja1.Tag = "pelaaja1";
         AddCollisionHandler(pelaaja1, "tahti", TormaaTahteen);
+        AddCollisionHandler(pelaaja1, "vartija", MeleeHyokkays);
         Add(pelaaja1);
     }
 
+
+
+
     void LisaaVartija(Vector paikka, double leveys, double korkeus)
     {
-        PhysicsObject vihuVartija = PhysicsObject.CreateStaticObject(leveys, korkeus);
-        FollowerBrain vartijanAivot = new FollowerBrain(pelaaja1);
+        Vihu vihuVartija = new Vihu(leveys, korkeus);
+
         vihuVartija.Position = paikka;
         vihuVartija.Mass = 4.0;
         vihuVartija.Image = vartijaKuva;
+        vihuVartija.Tag = "vartija";
         Add(vihuVartija);
+
+        vihuVartija.Brain = new FollowerBrain("pelaaja1");
+    }
+
+    void LisaaBossi(Vector paikka, double leveys, double korkeus)
+    {
+        Vihu vihuBoss = new Vihu(leveys, korkeus);
+
+        vihuBoss.Position = paikka;
+        vihuBoss.Mass = 4.0;
+        vihuBoss.Image = vartijaKuva;
+        vihuBoss.Tag = "pomo";
+        Add(vihuBoss);
+
+        vihuBoss.Brain = new RandomMoverBrain();
     }
 
     void LisaaNappaimet()
@@ -90,13 +123,15 @@ public class RoguePaladins : PhysicsGame
 
         Keyboard.Listen(Key.Left, ButtonState.Down, Liikuta, "Liikkuu vasemmalle", pelaaja1, -Nopeus);
         Keyboard.Listen(Key.Right, ButtonState.Down, Liikuta, "Liikkuu vasemmalle", pelaaja1, Nopeus);
-        Keyboard.Listen(Key.Up, ButtonState.Pressed, Hyppaa, "Pelaaja hyppää", pelaaja1, HyppyNopeus);
+        Keyboard.Listen(Key.Up, ButtonState.Pressed, Hyppaa, "Sankari hyppää", pelaaja1, HyppyNopeus);
+        Keyboard.Listen(Key.Up, ButtonState.Pressed, Hyppaa, "Sankari hyppää", pelaaja1, HyppyNopeus);
 
         ControllerOne.Listen(Button.Back, ButtonState.Pressed, Exit, "Poistu pelistä");
 
-        ControllerOne.Listen(Button.DPadLeft, ButtonState.Down, Liikuta, "Pelaaja liikkuu vasemmalle", pelaaja1, -Nopeus);
-        ControllerOne.Listen(Button.DPadRight, ButtonState.Down, Liikuta, "Pelaaja liikkuu oikealle", pelaaja1, Nopeus);
-        ControllerOne.Listen(Button.A, ButtonState.Pressed, Hyppaa, "Pelaaja hyppää", pelaaja1, HyppyNopeus);
+        ControllerOne.Listen(Button.DPadLeft, ButtonState.Down, Liikuta, "Sankari liikkuu vasemmalle", pelaaja1, -Nopeus);
+        ControllerOne.Listen(Button.DPadRight, ButtonState.Down, Liikuta, "Sankari liikkuu oikealle", pelaaja1, Nopeus);
+        ControllerOne.Listen(Button.A, ButtonState.Pressed, Hyppaa, "Sankari hyppää", pelaaja1, HyppyNopeus);
+        ControllerOne.Listen(Button.B, ButtonState.Pressed, Hyppaa, "Sankari hyökkää", pelaaja1, HyppyNopeus);
 
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
     }
@@ -116,19 +151,22 @@ public class RoguePaladins : PhysicsGame
         maaliAani.Play();
         MessageDisplay.Add("Keräsit tähden!");
         tahti.Destroy();
+        kenttaNro++;
+        SeuraavaKentta(kenttaNro);
     }
 
-    void MeleeHyokkays(PhysicsObject hahmo, PhysicsObject );
+    void MeleeHyokkays(PhysicsObject hahmo, PhysicsObject vihollinen)
+    {
+        vihollinen.Destroy();
+    }
 
-
-   /* void SeuraavaKentta()
+    void SeuraavaKentta(int kenttaNro)
     {
         ClearAll();
-        if (kenttaNro > 3) //Tähän kolmosen kohdalle tulee kenttien lukumäärä
-        else LuoKentta("kentta "+kenttaNro);
+        if (kenttaNro > 4) Exit(); //Tähän kolmosen kohdalle tulee kenttien lukumäärä
+            else LuoKentta(kenttaNro);
         //Jotta tämä toimisi, pitää jokaisen kenttätiedoston nimen
         //olla muotoa kentta ja heti perään numero esimerkiksi kentta4.txt
+    }
 
-        AsetaOhjaimet();
-    }*/
 }
---
+++ RoguePaladins/Vihu.cs
@@ -0,0 +1,13 @@
+﻿using Jypeli;
+
+class Vihu : PlatformCharacter
+{
+    private IntMeter elamaLaskuri = new IntMeter(3, 0, 3);
+    public IntMeter ElamaLaskuri { get { return elamaLaskuri; } }
+
+    public Vihu(double leveys, double korkeus)
+        : base(leveys, korkeus)
+    {
+        elamaLaskuri.LowerLimit += delegate { this.Destroy(); };
+    }
+}
